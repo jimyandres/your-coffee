99e0454107996882d3ed683803c9d1d882acc175
JohanQuiroga <quirogacj@utp.edu.co>
2017-09-13 23:15:10 -0500

    Updated home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,15 +3,23 @@ import {NavController} from 'ionic-angular';
 
 import {ProductPage} from '../product/product';
 import {SearchPage} from "../search/search";
+import {YourCoffeeWebServiceProvider} from "../../providers/your-coffee-web-service/your-coffee-web-service";
 
 @Component({
     selector: 'page-home',
-    templateUrl: 'home.html'
+    templateUrl: 'home.html',
+    providers: [YourCoffeeWebServiceProvider]
 })
 export class HomePage {
 
-    constructor(public navCtrl: NavController) {
+    public attributes: any;
+    public varieties: any;
+    public locations: any;
+    public price_range: Array<{min: Number, max: Number}> = [];
+    public products: Array<any> = [];
 
+    constructor(public navCtrl: NavController, private yourCoffeeService: YourCoffeeWebServiceProvider) {
+        this.loadProducts();
     }
 
     itemTapped(event, item) {
@@ -23,5 +31,16 @@ export class HomePage {
 
     searchPage() {
         this.navCtrl.push(SearchPage);
+    }
+
+    loadProducts() {
+        this.yourCoffeeService.load().subscribe((data) => {
+            this.attributes = data.attributes;
+            this.varieties = data.variedad_cafe;
+            this.locations = data.ubicaciones;
+            this.price_range = [{min: Number(data.min_price), max: Number(data.max_price)}];
+            this.products = data.products;
+        });
+        console.log(this.products);
     }
 }
