13b0208136f851620ca9162bbd85290345fa0067
jimyandres <jimyandres@utp.edu.co>
2017-09-18 16:53:33 -0500

    Added Modal-purchase page and some minor changes

---
+++ src/pages/modal-purchase/modal-purchase.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import {IonicPage, NavController, NavParams, ViewController} from 'ionic-angular';
+
+/**
+ * Generated class for the ModalPurchasePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-modal-purchase',
+  templateUrl: 'modal-purchase.html',
+})
+export class ModalPurchasePage {
+
+  admin: any = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              public viewCtrl: ViewController) {
+    this.admin = navParams.get('admin');
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ModalPurchasePage');
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
+}
