a27f304762759a39b8585962972730eecb18b531
jimyandres <jimyandres@utp.edu.co>
2017-09-23 15:51:45 -0500

    Provider page updated with Segment component

--- src/pages/provider/provider.ts
+++ src/pages/provider/provider.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { YourCoffeeWebServiceProvider } from "../../providers/your-coffee-web-service/your-coffee-web-service";
+import { ProductPage } from "../product/product";
+import { SearchPage } from "../search/search";
 
 /**
  * Generated class for the ProviderPage page.
@@ -12,25 +15,47 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-provider',
   templateUrl: 'provider.html',
+  providers: [YourCoffeeWebServiceProvider],
 })
 export class ProviderPage {
 
   provider: any = [];
   item: any;
   apiURL: string = '';
+  providerSegment: string = '';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              private yourCoffeeService: YourCoffeeWebServiceProvider) {
     this.apiURL = navParams.get('apiURL');
     this.item = navParams.get('item');
+    this.providerSegment = 'provider';
     this.loadData()
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ProviderPage');
+    console.log('ionViewDidLoad ProviderPage', this.provider);
   }
-
 
   loadData() {
     this.provider = this.item.provider;
+    this.provider.annosCafetal = this.provider.añosCafetal; // Reason: Letter ñ not supported!
+  }
+
+  itemTapped(event, item) {
+    // Get the product data to show
+    this.showProduct(item.idPublicacion);
+  }
+
+  showProduct(id) {
+    this.yourCoffeeService.product(id).subscribe((productInfo) => {
+      this.navCtrl.push(ProductPage, {
+        item: productInfo,
+        apiURL: this.apiURL
+      });
+    });
+  }
+
+  searchPage() {
+    this.navCtrl.push(SearchPage);
   }
 }
