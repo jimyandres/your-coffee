d5283bf3a906a7b5a7c835a6b6df14dcaaad5a66
JohanQuiroga <quirogacj@utp.edu.co>
2017-10-02 21:51:45 -0500

    Removed unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
 
 import {HomePage} from '../pages/home/home';
-import {ListPage} from '../pages/list/list';
 import {ProfilePage} from '../pages/profile/profile';
 import {AboutPage} from '../pages/about/about';
 import {SearchPage} from "../pages/search/search";
