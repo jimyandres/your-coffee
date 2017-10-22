a9e43f5543f072f5264709d61ccbfaf90857235d
jimyandres <jimyandres@utp.edu.co>
2017-10-22 11:13:48 -0500

    Profile Page updated (known bug: 'cause Storage module is async, it causes errors in the page's constructor when trying to define something that isn't finish to load)

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -5,7 +5,6 @@ import { LoadingController } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { YourCoffeeWebServiceProvider } from "../../providers/your-coffee-web-service/your-coffee-web-service";
-import { SearchPage } from "../search/search";
 import { LoginPage } from "../login/login";
 import { Storage } from '@ionic/storage';
 
@@ -32,8 +31,13 @@ export class ProfilePage {
   user: any = {};
   profileSegment: string = '';
   fieldsOptions: any = {};
+  paises: any = [];
+  departamentos: any = [];
+  ciudades: any = [];
 
   personalDataForm: FormGroup;
+  locationForm: FormGroup;
+  coffeePreferencesForm: FormGroup;
   submitAttempt: boolean = false;
   nextAttempt: boolean = false;
   errors: any = {};
@@ -79,14 +83,43 @@ export class ProfilePage {
         Validators.required])]
     });
 
+    this.locationForm = formBuilder.group({
+      pais: ['', Validators.compose([
+        Validators.maxLength(45),
+        Validators.required])],
+      departamento: ['', Validators.compose([
+        Validators.maxLength(45),
+        Validators.required])],
+      ciudad: ['', Validators.compose([
+        Validators.maxLength(45),
+        Validators.required])],
+      direccion: ['', Validators.compose([
+        Validators.maxLength(60),
+        Validators.required])],
+      direccionAuxiliar: ['', Validators.compose([
+        Validators.maxLength(60)])],
+      codigoPostal: ['', Validators.compose([
+        Validators.maxLength(10),
+        Validators.pattern('^\\d{0,10}$'),
+        Validators.required])]
+    });
+
+    let coffeePreferencesFields = {
+      idFrecuenciaCompraCafe: ['', Validators.compose([
+        Validators.required])],
+    };
+
     let attrs = this.fieldsOptions.attributes;
     // for( var i=0; i<attrs.length; i++) {
-      // formThreeFields[attrs[i].nombreAtributo] = [''];
+    //   coffeePreferencesFields[attrs[i].nombreAtributo] = [''];
     // }
+
+    this.coffeePreferencesForm = formBuilder.group(coffeePreferencesFields);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilePage');
+    this.country();
   }
 
   doRefresh(refresher: Refresher) {
@@ -101,8 +134,66 @@ export class ProfilePage {
     this.loading.present();
   }
 
+  getValues(obj) {
+    return Object.keys(obj).map(function(k) { return obj[k] });
+  }
+
+  country() {
+    this.yourCoffeeService.country().subscribe((data) => {
+      this.paises = this.getValues(data);
+    },
+    (err) => {
+      console.log(err);
+    });
+  }
+
+  departments(country) {
+    this.showLoader();
+    this.yourCoffeeService.departments(country).subscribe((data) => {
+      this.loading.dismiss();
+      this.departamentos = this.getValues(data);
+    },
+    (err) => {
+      this.loading.dismiss();
+      console.log(err);
+    });
+  }
+
+  cities(department) {
+    this.showLoader();
+    this.yourCoffeeService.cities(department).subscribe((data) => {
+      this.loading.dismiss();
+      this.ciudades = this.getValues(data);
+    },
+    (err) => {
+      this.loading.dismiss();
+      console.log(err);
+    });
+  }
+
+  countryChange(val: any) {
+    // console.log('Country Change:', val);
+    // this.departamentos = [];
+    // this.ciudades = [];
+    if(val && val != '') {
+      // this.user.departamento = '';
+      // this.user.ciudad = '';
+      this.departments(val);
+    }
+  }
+
+  departmentChange(val: any) {
+    // console.log('Department Change:', val);
+    // this.ciudades = [];
+    if(val && val != '') {
+      // this.slideTwoForm.controls.ciudad.value = '';
+      // this.ciudades = [];
+      this.cities(val);
+    }
+  }
+
   reloadUser(refresher?: Refresher) {
-    this.storage.ready().then(() => {
+    return Promise.all([this.storage.ready().then(() => {
       this.user = this.storage.get('user-token').then((token) => {
         if(token != null) {
           this.yourCoffeeService.user(token).then(
@@ -132,17 +223,21 @@ export class ProfilePage {
           );
         }
       });
-    });
+    }),
     this.yourCoffeeService.getRegister().subscribe((data) => {
       this.fieldsOptions = data;
     },
     (err) => {
       console.log(err);
-    });
+    })]);
   }
 
   deleteAccount() {
     console.log('Deleting Account');
+  }
+
+  updateProfile() {
+    console.log('Updating Account');
   }
 
 }
